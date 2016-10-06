e1190fae933e8b369e77b0599284a0bfbb80912c
Daniel Arango <danielarango990@gmail.com>
2016-10-06 00:12:36 -0500

    archivos publicos de la web

---
+++ public/js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    init();
+    Scroll();
+    $("body").jpreLoader();
+});
+
+function init(){
+    $('.button-collapse').sideNav({
+      menuWidth: 300,
+      edge: 'left',
+      closeOnClick: true 
+    });
+    $('.collapsible').collapsible();
+} 
+
+function Scroll(){
+    $("#menu-lateral li a").on("click",function(){
+    var seccion = $(this).attr("data-menu");
+    $("body").animate({ scrollTop: $("#item-"+seccion).offset().top},1000);
+  });
+
+    $("#scroll").on('click', function(){
+    	$("body").animate({ scrollTop: $("#item-1").offset().top},1000);
+    });
+}
+/*
+if(window.location.hreft.indexOf("http://ww.tumblr.com")==1){
+	window.location.replace("url"+window.location.pathname);
+}
+*/
